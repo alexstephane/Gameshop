cf87f47fb8ca9dde659d3109ac168336040a8209
alexstephane <alexngundji@gmail.comm.com>
2020-09-27 12:24:54 -0400

    comepleted the game controller file and listing controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,50 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+  
+  def show 
+    @game = Game.find(params[:id])
+  end
   def new
+    @game = Game.new
 
   end
 
   def create
+    @game= Game.new(game_strong_params)
+    if @game.save
+        redirect_to game_path(@game)
+    else
+        render :new
+    end  
 
   end
 
   def edit
+    @game = Game.find(params[:id])
+    render :edit
 
   end
 
   def update
+    game= Game.find(params[:id])
+        @games= Game.update(game_strong_params)
+        if @game.valid?
+            redirect_to game_path(@game)
+        else   
+            render :edit
+        end  
 
   end
 
-  def delete
+  def detroy
+    User.destroy(params[:id])
+    redirect_to games_path
 
+  end
+
+  def game_strong_params
+    params.require(:game).permit(:name, :developer, :release_date, :rating)
   end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,21 +1,50 @@
 class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+  end
+  
+  def show 
+    @listing = Listing.find(params[:id])
+  end
   def new
+    @listing = Listing.new
 
   end
 
   def create
+    @listing= Listing.new(listing_strong_params)
+    if @listing.save
+        redirect_to listing_path(@listing)
+    else
+        render :new
+    end  
 
   end
 
   def edit
+    @listing = Listing.find(params[:id])
+    render :edit
 
   end
 
   def update
+    listing= Listing.find(params[:id])
+        @listing= Listing.update(listing_strong_params)
+        if @listing.valid?
+            redirect_to listing_path(@game)
+        else   
+            render :edit
+        end  
 
   end
 
-  def delete
+  def detroy
+    Listing.destroy(params[:id])
+    redirect_to listings_path
 
   end
-end
+
+  def listing_strong_params
+    params.require(:listing).permit(:game_id, :user_id, :console_type_id, :quality, :condition, :listed_price, :availability)
+  end
+  end
